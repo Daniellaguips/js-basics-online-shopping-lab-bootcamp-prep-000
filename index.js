0c0dff2c9b5f79734330f5e6f7ac98a5b6bd8a39
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-13 20:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,6 +69,6 @@ function placeOrder(cardNumber) {
   } else { 
      var  newTotal = `${total()}`
      cart = [] 
-     return `Your total cost is $${newTotal()}, which will be charged to the card ${cardNumber}. `
+     return `Your total cost is $${newTotal}, which will be charged to the card ${cardNumber}. `
   }
 }
