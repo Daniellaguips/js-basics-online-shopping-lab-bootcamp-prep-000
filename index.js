cafc6e84128b46ee2f11af64a5aba35d40cc9099
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-13 20:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
   return "Sorry, we don't have a credit card on file for you."
   } else { 
-     var total = parseInt(total())
+     total()
      cart = [] 
-     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}. `
+     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}. `
   }
 }
